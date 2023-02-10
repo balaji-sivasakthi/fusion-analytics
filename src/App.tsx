69bdf475b79f9fe2bee481ed082793bff60f7216
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-02-10 17:56:51 +0530

    feat: project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './views/app/Dashboard';
+import Login from './views/auth/Login';
+
+//Theme files
+import darkTheme from './config/theme/darkTheme';
+import lightTheme from './config/theme/lightTheme';
+
+import  ThemeProvider  from '@mui/material/styles/ThemeProvider';
 function App() {
+  const [dark, setdark] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <ThemeProvider theme={dark?darkTheme:lightTheme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Dashboard />} />
+          <Route path={"/login"} element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+
   );
 }
 
