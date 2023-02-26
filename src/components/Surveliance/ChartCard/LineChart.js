04041713a12b795ac71664db6937326c8fbba899
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-02-27 01:24:45 +0530

    feat new features

--- src/components/Surveliance/ChartCard/LineChart.js
+++ src/components/Surveliance/ChartCard/LineChart.js
@@ -8,56 +8,61 @@ function LineChart() {
 
   const options = {
     series: [{
-      name: "Desktops",
+      name: "User",
       data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-  }],
-  options: {
-    chart: {
-      height: 350,
-      type: 'line',
-      zoom: {
+    },
+    {
+      name: "Vehicle",
+      data: [1, 42, 25, 21, 39, 52, 69, 71, 168]
+    }
+    ],
+    options: {
+      chart: {
+
+        type: 'line',
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
         enabled: false
+      },
+      stroke: {
+        curve: 'straight'
+      },
+
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5
+        },
+      },
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
       }
     },
-    dataLabels: {
-      enabled: false
-    },
-    stroke: {
-      curve: 'straight'
-    },
-    
-    grid: {
-      row: {
-        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-        opacity: 0.5
-      },
-    },
-    xaxis: {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-    }
-  },
 
-};
+  };
 
 
 
-return (
-  <Card>
+  return (
+    <Card sx={{ height: "100%" }}>
 
-    <CardHeader title={"Line Chart"} />
-    <Typography px={2}>Based on selected Stores</Typography>
-    <CardContent>
-      <Chart
-                    options={options?.options}
-                    series={options.series}
-                    type="line"
-                    width={"100%"}
-                    height={"200px"}
-                    
-                />
-    </CardContent>
+      <CardHeader title={"User/Vechicle"} />
+      <Typography px={2}>Based on selected Stores</Typography>
+      <CardContent>
+        <Chart
+          options={options?.options}
+          series={options.series}
+          type="line"
+          width={"100%"}
 
-  </Card>
-)
+
+        />
+      </CardContent>
+
+    </Card>
+  )
 }
 export default LineChart
